242e8b18fcc00daf303375817a90539f577e98a2
Angel Lozano <angel_ivan_5@outlook.com>
2023-12-05 23:55:01 -0600

    Added x power n recursive function

--- SomeFunctionTests/recursive.py
+++ SomeFunctionTests/recursive.py
@@ -17,11 +17,20 @@ def sumN_NatNum(n):
         return n
     return n + sumN_NatNum(n - 1)
 
+def xPOWn(x, n):
+    if n == 1:
+        return x
+    return x*xPOWn(x, n - 1)
+
+
 test_num  = 4
 test_numx = 0
+test_x = 2
+test_n = 8
 
 #countdown(10)
 print("Fibonacci(" + str(test_num) + ")=" + str(fibonacci(test_num)))
 print(str(test_num) + "!=" + str(factorial(test_num)))
 print(str(test_numx) + "!=" + str(factorial(test_numx)))
 print("Sum of first " + str(test_num) + " natural numbers= " + str(sumN_NatNum(test_num)))
+print(str(test_x) + "^" + str(test_n) + "= " + str(xPOWn(test_x, test_n)))
