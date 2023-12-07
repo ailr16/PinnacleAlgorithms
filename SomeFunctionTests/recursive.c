8eaa0728bbff343dce6192be9355217b742269bc
Angel Lozano <angel_ivan_5@outlook.com>
2023-12-06 18:27:22 -0600

    Created source file with command to compile

---
+++ SomeFunctionTests/recursive.c
@@ -0,0 +1,12 @@
+//Works for numbers smaller than 0xFFFF
+
+#include <stdio.h>
+
+/*
+    gcc -Wall -o SomeFunctionTests/recursive SomeFunctionTests/recursive.c -g  
+*/
+
+int main(void)
+{
+    return 0;
+}
