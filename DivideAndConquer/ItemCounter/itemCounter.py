1f94d94dc4d2987e6c8871455297bfa437f628a7
Angel Lozano <angel_ivan_5@outlook.com>
2023-12-09 00:47:53 -0600

    Corrected list global manipulation

--- DivideAndConquer/ItemCounter/itemCounter.py
+++ DivideAndConquer/ItemCounter/itemCounter.py
@@ -1,11 +1,12 @@
 def countItems(array:list):
     if len(array) == 0:
         return 0
-    array.pop()
-    return 1 + countItems(array)
+    return 1 + countItems(array[1:])
 
 test_array1 = [1, 1, 1]
 test_array2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+test_array3 = [1, 1, 1, 1, 1, 1, 20000]
 
 print("Items= " + str(countItems(test_array1)))
 print("Items= " + str(countItems(test_array2)))
+print("Items= " + str(countItems(test_array3)))
