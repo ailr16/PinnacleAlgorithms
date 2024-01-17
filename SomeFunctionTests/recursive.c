fcef5d7bd39d5fa390e5c5fddf33e937e895dcad
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-17 00:56:11 -0600

    fibonacci iterative

--- SomeFunctionTests/recursive.c
+++ SomeFunctionTests/recursive.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 
 /*
-    gcc -Wall -o SomeFunctionTests/recursive SomeFunctionTests/recursive.c -g  
+    gcc -Wall -o SomeFunctionTests/recursive SomeFunctionTests/recursive.c -g && ./SomeFunctionTests/recursive
 */
 
 void countdown( int n );
 int fibonacci( int n );
+int fibonacciI( int n );
 int factorial( int n );
 int sumNnat( int n);
 int xpown( int x, int n );
@@ -15,12 +16,13 @@ int xpown( int x, int n );
 int main( void )
 {
     countdown( 16 );
-    printf("Fibonacci (7)= %d\n", fibonacci( 7 ));
+    printf("Fibonacci (12)= %d\n", fibonacci( 12 ));
     printf("7!= %d\n", factorial( 7 ));
     printf("0!= %d\n", factorial( 0 ));
     printf("Sum of 7 first natural numbers= %d\n", sumNnat( 7 ));
     printf("2^16= %d\n", xpown( 2, 16 ));
     printf("3^4= %d\n", xpown( 3, 4 ));
+    printf("Fibonacci iterative: %d\n", fibonacciI( 12 ));
     return 0;
 }
 
@@ -41,6 +43,22 @@ int fibonacci( int n )
     }
 
     return ( fibonacci( n - 1 ) + fibonacci( n - 2 ) );
+}
+
+int fibonacciI( int n )
+{
+    int i = 0;
+    int j = 1;
+    int aux = 0;
+
+    for( int k = 0; k < n - 1 ; k++ )
+    {
+        aux = i + j;
+        i = j;
+        j = aux;
+    }
+    
+    return j;
 }
 
 int factorial( int n )
