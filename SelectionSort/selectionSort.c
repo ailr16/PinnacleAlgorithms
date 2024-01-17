282f0619fbdfa60399c49031a5dcfa460c714127
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-17 00:12:25 -0600

    Selection sort in C revisited

--- SelectionSort/selectionSort.c
+++ SelectionSort/selectionSort.c
@@ -1,41 +1,23 @@
-//Works for numbers smaller than 0xFFFF
-
 #include <stdio.h>
 
 /*
-    gcc -Wall -o SelectionSort/selectionSort SelectionSort/selectionSort.c -g
+    gcc -Wall -o SelectionSort/selectionSort SelectionSort/selectionSort.c -g && ./SelectionSort/selectionSort
 */
 
-int findSmallest( int *dataArray, int arraySize );
-int selectionSort( int *dataArray, int *resultArray, int arraySize );
-int popElement(int *dataArray, int arraySize, int elementIndex);
-void printArray(int *dataArray, int arraySize);
-
-int main(void)
+void swap( int *a, int *b )
 {
-    int dataToSort[] = { 12, 5, 34, 1, 16, 1999, 99, 48, -1, -1};
-    int arraySize    = sizeof( dataToSort ) / sizeof( dataToSort[0] );
-
-    int sortedData[arraySize];
-
-    printArray(dataToSort, arraySize);
-
-    selectionSort( dataToSort, sortedData, arraySize );
-
-    printArray(sortedData, arraySize);
-  
-
-    return 0;
+    int aux = *a;
+    *a = *b;
+    *b = aux;
 }
 
-
-int findSmallest( int *dataArray, int arraySize )
+int findSmallest( int *dataArray, int startIndex, int arraySize )
 {
-    int smallest       = dataArray[ 0 ];
-    int smallest_index = 0;
+    int smallest       = dataArray[ startIndex ];
+    int smallest_index = startIndex;
     int i              = 0;
 
-    for( i = 0; i < arraySize; i++ )
+    for( i = startIndex; i < arraySize; i++ )
     {
         if(dataArray[i] < smallest)
         {
@@ -48,45 +30,20 @@ int findSmallest( int *dataArray, int arraySize )
 }
 
 
-int selectionSort( int *dataArray, int *resultArray, int arraySize )
+int selectionSort( int *dataArray, int arraySize )
 {
     int i = 0;
-    int smallest = 0;
+    int smallestIndex = 0;
 
     for( i = 0; i < arraySize; i++ )
     {
-        smallest       = findSmallest( dataArray, arraySize );
-        resultArray[i] = popElement( dataArray, arraySize, smallest );
+        smallestIndex = findSmallest( dataArray, i, arraySize );
+        swap( &dataArray[smallestIndex], &dataArray[i] );
     }
 
     return 0;
 }
 
-
-int popElement(int *dataArray, int arraySize, int elementIndex)
-{
-    int elementValue = 0;
-    int i            = 0;
-
-    if(elementIndex < arraySize)
-    {
-        elementValue = dataArray[elementIndex];
-
-        for( i = elementIndex; i < arraySize; i ++ )
-        {
-            if( (i + 1) < arraySize )
-            {
-                dataArray[ i ] = dataArray[i + 1];
-            }
-            else
-            {
-                dataArray[ i ] = 0xFFFF;
-            }
-        }
-    }
-
-    return elementValue;
-}
 
 void printArray(int *dataArray, int arraySize)
 {
@@ -95,4 +52,30 @@ void printArray(int *dataArray, int arraySize)
         printf("%d ", dataArray[i]);
     }
     printf("\n");
+}
+
+int main(void)
+{
+    int dataToSort[10] = { 12, 5, 34, 1, 16, 1999, 99, 48, -1, -1};
+    int array2[40] = { 6717, 7811, 4833, 2017, 7240, 8425, 1848, 8343,
+                     4164, 6041, 5741, 4891, 9364, 7811, 318,  8208,
+                     8144, 6556, 7906, 8964, 5266, 1951, 3594, 4165,
+                     3496, 7897, 3745, 6745, 5301, 3362, 3914, 7683,
+                     9164, 6216, 6887, 5492, 1961, 9719, 3717, 7484 };
+
+    printf("Unsorted: ");
+    printArray(dataToSort, 10);
+    selectionSort( dataToSort, 10 );
+    printf("Sorted: ");
+    printArray(dataToSort, 10);
+
+    printf("Unsorted: ");
+    printArray(array2, 40);
+    selectionSort( array2, 40 );
+    printf("Sorted: ");
+    printArray(array2, 40);
+  
+  
+
+    return 0;
 }
