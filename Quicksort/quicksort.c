d57afd3a249be9b669239513b5dc514d71457d67
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-16 18:49:12 -0600

    Quicksort in C finished

--- Quicksort/quicksort.c
+++ Quicksort/quicksort.c
@@ -12,27 +12,33 @@ void swap( int *a, int *b )
     *b = aux;
 }
 
-int quicksort( int *a, int size )
+int partition( int *a, int start, int end )
 {
-    if( size == 0 )
-        return 1;
+    int pivot = a[ end ];
+    int i = start - 1;
 
-    int pivotIndex = size - 1;
-    int swapIndex = 0;
-    for( int i = 0; i < pivotIndex; i++ )
+    for( int j = start; j <= end; j++ )
     {
-        if( a[ i ] <= a[ pivotIndex ] )
+        if( a[ j ] < pivot )
         {
-            swapIndex++;
-            if( a[ i ] > a[ swapIndex ] )
-            {
-                swap( &a[ i ], &a[ swapIndex ] );
-            }
+            i++;
+            swap( &a[ i ], &a[ j ] );
         }
     }
-    
-    quicksort( a, size - 1 );
-    return 0;
+
+    swap( &a[ i + 1 ], &a[ end ] );
+
+    return (i + 1);
+}
+
+void quicksort( int *a, int start, int end )
+{
+    if( start < end )
+    {
+        int pi = partition( a, start, end );
+        quicksort( a, start, pi - 1 );      // Lower values
+        quicksort( a, pi + 1, end );        // Higher values
+    }
 }
 
 void printArray( int *a, int size )
@@ -48,14 +54,20 @@ int main( void )
     int array1[8] = {1, 3, 5, 7, 2, 4, 6, 8};
     int array2[8] = {2, 4, 6, 8, 1, 3, 5, 7};
     int array3[10] = {3, 2, 5, 0, 1, 8, 7, 6, 9, 4};
+    int array4[8] = {2, 4, -99, 1, 1, 3, 5, 7};
+    int array5[5] = {1, 3, 5, 7, 2};
 
-    quicksort( array1, 8 );
-    quicksort( array2, 8 );
-    quicksort( array3, 10 );
+    quicksort( array1, 0, 7 );
+    quicksort( array2, 0, 7 );
+    quicksort( array3, 0, 9 );
+    quicksort( array4, 0, 7 );
+    quicksort( array5, 0, 4 );
 
     printArray( array1, 8 );
     printArray( array2, 8 );
     printArray( array3, 10 );
+    printArray( array4, 8 );
+    printArray( array5, 5 );
 
     return 0;
 }
