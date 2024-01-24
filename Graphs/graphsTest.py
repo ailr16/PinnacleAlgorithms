d708709db046ec66040eae54e9daa8bee699af4b
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-23 22:05:15 -0600

    Implemented a graph (Chatper 6)

---
+++ Graphs/graphsTest.py
@@ -0,0 +1,18 @@
+graph = {}
+
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+print( graph["you"] )
+print( graph["bob"] )
+print( graph["alice"] )
+print( graph["anuj"] )
+print( graph["peggy"] )
+print( graph["thom"] )
+print( graph["jonny"] )
