b0dadd17a080d80b58bb844384400c68b73275bd
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-24 22:33:53 -0600

    Implemented a graph algorithm

--- Graphs/graphsTest.py
+++ Graphs/graphsTest.py
@@ -1,18 +1,29 @@
+from collections import deque
+
 graph = {}
 
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
 graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
 graph["anuj"] = []
 graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
 
+def person_is_seller(name):
+    return name[-1] == 'm'
 
-print( graph["you"] )
-print( graph["bob"] )
-print( graph["alice"] )
-print( graph["anuj"] )
-print( graph["peggy"] )
-print( graph["thom"] )
-print( graph["jonny"] )
+def graph_algo():
+    search_queue = deque()
+    search_queue += graph["you"]
+    while search_queue:
+        person = search_queue.popleft()
+        if person_is_seller(person):
+            print(person + " is a mango seller")
+            return True
+        else:
+            search_queue += graph[person]
+    return False
+
+graph_algo()
