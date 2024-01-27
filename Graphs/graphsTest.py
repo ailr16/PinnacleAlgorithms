8ba58cbb98b586fb3c299a6522300a2ed9acca1a
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-26 22:52:18 -0600

    Updated algorithm for graph search

--- Graphs/graphsTest.py
+++ Graphs/graphsTest.py
@@ -12,18 +12,21 @@ graph["thom"] = []
 graph["jonny"] = []
 
 def person_is_seller(name):
-    return name[-1] == 'm'
+    return name[-1] == 'j'
 
-def graph_algo():
+def search(name):
     search_queue = deque()
-    search_queue += graph["you"]
+    search_queue += graph[name]
+    searched = []
     while search_queue:
         person = search_queue.popleft()
-        if person_is_seller(person):
-            print(person + " is a mango seller")
-            return True
-        else:
-            search_queue += graph[person]
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
     return False
 
-graph_algo()
+search("you")
