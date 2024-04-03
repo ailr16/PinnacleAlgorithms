573b263a114b005dc1e39012bc4fdf178ed69e06
Angel Lozano <angel_ivan_5@outlook.com>
2024-04-02 23:52:15 -0600

    Solved problem 2, sum two linked lists

--- leetCode/leetCode/main.c
+++ leetCode/leetCode/main.c
@@ -35,9 +35,89 @@ void twoSumTest(void) {
 
 	result = twoSum(nums, 3, 6, &returnSize);
 	printf("%d %d\n", result[0], result[1]);
+	free(result);
+}
+
+
+/**
+ * Problem 2
+ */
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+	struct ListNode* returnValue = NULL;
+	struct ListNode* temp = NULL;
+
+	struct ListNode* i1 = l1;
+	struct ListNode* i2 = l2;
+
+	char carry = 0;
+	int sum = 0;
+
+	while (i1 || i2 || carry) {
+		if (i1) {
+			sum += i1->val;
+			i1 = i1->next;
+		}
+
+		if (i2) {
+			sum += i2->val;
+			i2 = i2->next;
+		}
+
+		if (carry) {
+			sum++;
+			carry = 0;
+		}
+
+		if (sum > 9) {
+			carry = 1;
+			sum = sum % 10;
+		}
+
+		struct ListNode* newNode = malloc(sizeof(struct ListNode));
+		if (newNode) newNode->val = sum;
+
+		if (returnValue) {
+			temp->next = newNode;
+			temp = newNode;
+		}
+		else {
+			returnValue = temp = newNode;
+		}
+
+		sum = 0;
+	}
+	
+	if (temp) temp->next = NULL;
+	return returnValue;
+}
+
+void addTwoNumbersTest(void) {
+	struct ListNode* result;
+
+	struct ListNode l1d = { .val = 1, .next = NULL };
+	struct ListNode l1  = { .val = 1, .next = &l1d };
+
+	struct ListNode l2d = { .val = 9, .next = NULL };
+	struct ListNode l2c = { .val = 9, .next = &l2d };
+	struct ListNode l2b = { .val = 9, .next = &l2c };
+	struct ListNode l2  = { .val = 9, .next = &l2b };
+
+	result = addTwoNumbers(&l1, &l2);
+	
+	struct ListNode* i = result;
+
+	while (i) {
+		printf("%d\n", i->val);
+		i = i->next;
+	}
 }
 
 int main() {
-	twoSumTest();
+	addTwoNumbersTest();
 	return 0;
 }
