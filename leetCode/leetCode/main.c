92ad084da934e69b19500e90a28fd3a88a69a509
Angel Lozano <angel_ivan_5@outlook.com>
2024-04-14 23:44:04 -0600

    First approach problem 29

--- leetCode/leetCode/main.c
+++ leetCode/leetCode/main.c
@@ -309,7 +309,39 @@ void removeDuplicatesTest(void) {
 	printArray(array3, array3Size);
 }
 
+
+/**
+ * Problem 29
+ * Divide two integers
+ */
+int divide(int dividend, int divisor) {
+	bool sign = (dividend < 0) ^ (divisor < 0);
+	
+	if (divisor == 0) return 0;
+	if ((dividend == INT_MAX && divisor == 1) || (divisor == -INT_MAX && dividend == -1)) return INT_MAX;
+	if ((dividend == INT_MIN && divisor == 1) || (divisor == -INT_MAX && dividend == -1)) return INT_MIN;
+
+	unsigned result = 0;
+	int udividend = abs(dividend);
+	int udivisor = abs(divisor);
+
+	while (udividend >= udivisor) {
+		udividend -= udivisor;
+		result++;
+	}
+
+	if (sign) result *= -1;
+
+	return result;
+}
+
+void divideTest(void) {
+	printf("%d\n", divide(-10, -3));
+	printf("%d\n", divide(-2147483647, -1));
+	printf("%d\n", divide(2147483647, -1));
+}
+
 int main() {
-	removeDuplicatesTest();
+	divideTest();
 	return 0;
 }
