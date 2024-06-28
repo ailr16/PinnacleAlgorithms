7b0d8864a5f02b9d30b7ba70cc8c64537b14b766
Angel Lozano <angel_ivan_5@outlook.com>
2024-06-27 21:59:23 -0600

    Test reversing list

--- leetCode/leetCode/main.c
+++ leetCode/leetCode/main.c
@@ -341,7 +341,44 @@ void divideTest(void) {
 	printf("%d\n", divide(2147483647, -1));
 }
 
+
+/**
+ * Problem 206
+ * Reverse Linked List
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+	struct ListNode* prev = head;
+	struct ListNode* i = head->next;
+
+	while (i) {
+		printf("prev=%d current=%d next=%d\n", prev->val, i->val, i->next->val);
+		prev = prev->next;
+		i = i->next;
+	}
+	
+}
+
+void reverseListTest(void) {
+	struct ListNode* result;
+
+	struct ListNode l1f = { .val = 4, .next = NULL };
+	struct ListNode l1e = { .val = 4, .next = &l1f };
+	struct ListNode l1d = { .val = 4, .next = &l1e };
+	struct ListNode l1c = { .val = 3, .next = &l1d };
+	struct ListNode l1b = { .val = 2, .next = &l1c };
+	struct ListNode l1 = { .val = 1, .next = &l1b };
+
+	struct ListNode l2d = { .val = 9, .next = NULL };
+	struct ListNode l2c = { .val = 9, .next = &l2d };
+	struct ListNode l2b = { .val = 9, .next = &l2c };
+	struct ListNode l2 = { .val = 9, .next = &l2b };
+
+	struct ListNode* i = &l1;
+
+	result = reverseList(&l1);
+}
+
 int main() {
-	divideTest();
+	reverseListTest();
 	return 0;
 }
