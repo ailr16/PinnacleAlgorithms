98fa24c6263d5e65ede993dca22c4841709ddbe6
ailr16 <angel_ivan_5@outlook.com>
2024-12-26 22:28:44 -0600

    Added tests for longest substring problem

--- leetCode/test/testsSrc/test_longestSubStr.c
+++ leetCode/test/testsSrc/test_longestSubStr.c
@@ -7,10 +7,38 @@ void TEST__longestSubStr_2char(void) {
     printf("%d\n", lengthOfLongestSubstring(str) );
 }
 
+void TEST__longestSubStr_3char(void) {
+	char str[] = "abcabcbb";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
+void TEST__longestSubStr_1char(void) {
+	char str[] = "bbbbb";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
+void TEST__longestSubStr_6char(void) {
+	char str[] = "hijjjjjjtjjb";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
+
 void Register_tests_longestSubStr(void){
     SingleTestBlock test;
 
 	test.function = TEST__longestSubStr_2char;
 	strcpy( test.testName, "Longest substring. Expected: 2" );
+	registerTest( &htest, &test );
+
+    test.function = TEST__longestSubStr_3char;
+	strcpy( test.testName, "Longest substring. Expected: 3" );
+	registerTest( &htest, &test );
+
+    test.function = TEST__longestSubStr_1char;
+	strcpy( test.testName, "Longest substring. Expected: 1" );
+	registerTest( &htest, &test );
+
+	test.function = TEST__longestSubStr_6char;
+	strcpy( test.testName, "Longest substring. Expected: 6" );
 	registerTest( &htest, &test );
 }
