28b6b1063d4e9a454552c3cb856b50f0e36c957e
ailr16 <angel_ivan_5@outlook.com>
2024-12-30 16:39:40 -0600

    Working with sub sequences

--- leetCode/src/longestSubStr.c
+++ leetCode/src/longestSubStr.c
@@ -1,5 +1,25 @@
 #include "longsetSubStr.h"
 
 int lengthOfLongestSubstring(char* s){
-    return 0;
+    int maxCharCount = 0;
+    int currentCharCount;
+    char *i;
+    __uint128_t var = 0;
+    
+    i = s;
+    currentCharCount = 0;
+
+    while( *i != '\0' ){
+        if( ( var & ((__uint128_t)1 << *i) ) && 1 ){
+            currentCharCount = 0;
+            var = 0;
+        }
+        var ^= ( (__uint128_t)1 << *i );
+
+        currentCharCount++;
+        if( currentCharCount > maxCharCount ) maxCharCount = currentCharCount;
+        i++;
+    }
+
+    return maxCharCount;
 }
--- leetCode/test/testsSrc/test_longestSubStr.c
+++ leetCode/test/testsSrc/test_longestSubStr.c
@@ -2,12 +2,12 @@
 #include "solutions.h"
 #include "tests.h"
 
-void TEST__longestSubStr_2char(void) {
+void TEST__longestSubStr_3char1(void) {
 	char str[] = "Hello";
     printf("%d\n", lengthOfLongestSubstring(str) );
 }
 
-void TEST__longestSubStr_3char(void) {
+void TEST__longestSubStr_3char2(void) {
 	char str[] = "abcabcbb";
     printf("%d\n", lengthOfLongestSubstring(str) );
 }
@@ -17,20 +17,36 @@ void TEST__longestSubStr_1char(void) {
     printf("%d\n", lengthOfLongestSubstring(str) );
 }
 
-void TEST__longestSubStr_6char(void) {
+void TEST__longestSubStr_3char3(void) {
 	char str[] = "hijjjjjjtjjb";
     printf("%d\n", lengthOfLongestSubstring(str) );
 }
+
+void TEST__longestSubStr_6char(void) {
+	char str[] = "Helabc";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
+void TEST__longestSubStr_noString(void) {
+	char str[] = "";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
+void TEST__longestSubStr_space(void) {
+	char str[] = " ";
+    printf("%d\n", lengthOfLongestSubstring(str) );
+}
+
 
 
 void Register_tests_longestSubStr(void){
     SingleTestBlock test;
 
-	test.function = TEST__longestSubStr_2char;
-	strcpy( test.testName, "Longest substring. Expected: 2" );
+	test.function = TEST__longestSubStr_3char1;
+	strcpy( test.testName, "Longest substring. Expected: 3" );
 	registerTest( &htest, &test );
 
-    test.function = TEST__longestSubStr_3char;
+    test.function = TEST__longestSubStr_3char2;
 	strcpy( test.testName, "Longest substring. Expected: 3" );
 	registerTest( &htest, &test );
 
@@ -38,7 +54,19 @@ void Register_tests_longestSubStr(void){
 	strcpy( test.testName, "Longest substring. Expected: 1" );
 	registerTest( &htest, &test );
 
+	test.function = TEST__longestSubStr_3char3;
+	strcpy( test.testName, "Longest substring. Expected: 3" );
+	registerTest( &htest, &test );
+
 	test.function = TEST__longestSubStr_6char;
 	strcpy( test.testName, "Longest substring. Expected: 6" );
+	registerTest( &htest, &test );
+
+	test.function = TEST__longestSubStr_noString;
+	strcpy( test.testName, "No string. Expected: 0" );
+	registerTest( &htest, &test );
+
+	test.function = TEST__longestSubStr_space;
+	strcpy( test.testName, "Just a space. Expected: 1" );
 	registerTest( &htest, &test );
 }
