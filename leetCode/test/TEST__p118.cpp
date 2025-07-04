e8465b9d024d3dc3d93009f997014c05cf570ffb
ailr16 <angel_ivan_5@outlook.com>
2025-07-03 20:24:02 -0600

    Added test to pascal

--- leetCode/test/TEST__p118.cpp
+++ leetCode/test/TEST__p118.cpp
@@ -55,3 +55,17 @@ TEST(Problem118Test, testSolution_4rows){
         }
     }
 }
+
+TEST(Problem118Test, testSolution_5rows){
+    p118::Solution pascal;
+    std::vector<std::vector<int>> result;
+    std::vector<std::vector<int>> expected = {{1},{1,1},{1,2,1},{1,3,3,1},{1,4,6,4,1}};
+    result = pascal.generate(5);
+
+    EXPECT_EQ(result.size(), expected.size());
+    for(int row = 0; row < expected.size(); row++){
+        for(int coeff = 0; coeff < expected[row].size(); coeff++){
+            EXPECT_EQ(expected[row][coeff], result[row][coeff]);
+        }
+    }
+}
