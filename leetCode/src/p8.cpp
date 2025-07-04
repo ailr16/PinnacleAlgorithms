d4226e3d307263264dc5d59c168d03f6c0b02868
ailr16 <angel_ivan_5@outlook.com>
2025-07-04 02:00:13 -0600

    first algo approach

--- leetCode/src/p8.cpp
+++ leetCode/src/p8.cpp
@@ -1,5 +1,27 @@
 #include "p8.hpp"
 
 int p8::Solution::myAtoi(std::string s){
-    return 0;
+    int result = 0;
+    bool signFlag = false;
+    bool startFlag = false;
+
+    for(char c : s){
+        if(c == ' ') continue;
+        if(c == '-') signFlag = true;
+        if(!startFlag){
+            if(c > '0') startFlag = true;
+        }
+        if(startFlag){
+            if(c >= '0' && c <= '9'){
+                result *= 10;
+                result += c - '0';
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    if(signFlag) result *= -1;
+    return result;
 }
--- leetCode/test/TEST__p8.cpp
+++ leetCode/test/TEST__p8.cpp
@@ -4,5 +4,15 @@
 
 TEST(Problem8Test, testSolution){
     p8::Solution test;
-    EXPECT_EQ(0, 0);
+
+    EXPECT_EQ(test.myAtoi("    -542"), -542);
+    EXPECT_EQ(test.myAtoi("-542"), -542);
+    EXPECT_EQ(test.myAtoi("      542"), 542);
+    EXPECT_EQ(test.myAtoi("    -0000542"), -542);
+    EXPECT_EQ(test.myAtoi("0000542"), 542);
+    EXPECT_EQ(test.myAtoi("000054200"), 54200);
+    EXPECT_EQ(test.myAtoi("00005420a"), 5420);
+    EXPECT_EQ(test.myAtoi(""), 0);
+    EXPECT_EQ(test.myAtoi("abc"), 0);
+
 }
