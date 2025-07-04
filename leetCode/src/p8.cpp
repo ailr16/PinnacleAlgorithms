b9d3d671cb7ac46fa03c5c82ffc3c82f95a70917
ailr16 <angel_ivan_5@outlook.com>
2025-07-04 01:39:46 -0600

    ready environment for problem 8

---
+++ leetCode/src/p8.cpp
@@ -0,0 +1,5 @@
+#include "p8.hpp"
+
+int p8::Solution::myAtoi(std::string s){
+    return 0;
+}
---
+++ leetCode/test/TEST__p8.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "p8.hpp"
+
+TEST(Problem8Test, testSolution){
+    p8::Solution test;
+    EXPECT_EQ(0, 0);
+}
