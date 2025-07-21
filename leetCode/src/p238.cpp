65a82e472f72050f5461e11fce013af5fa9afcc0
ailr16 <angel_ivan_5@outlook.com>
2025-07-20 23:49:08 -0600

    improved performance

--- leetCode/src/p238.cpp
+++ leetCode/src/p238.cpp
@@ -2,21 +2,18 @@
 
 std::vector<int> p238::Solution::productExceptSelf(std::vector<int>& nums) {
     std::vector<int> result(nums.size());
-    std::vector<int> result2(nums.size());
 
     int i;
-    int j;
 
     result[0] = 1;
-    for(i = 1, j = 0; i < nums.size(); i++, j++)
-        result[i] = result[j] * nums[i - 1];
+    for(i = 1; i < nums.size(); i++)
+        result[i] = result[i - 1] * nums[i - 1];
 
-    result2[nums.size() - 1] = 1;
-    for(i = nums.size() - 2, j = nums.size() - 1; i >= 0; i--, j--)
-        result2[i] = nums[i + 1] * result2[j];
-
-    for(i = 0; i < nums.size(); i++)
-        result[i] = result[i] * result2[i];
+    int carry = 1;
+    for(i = nums.size() - 2; i >= 0; i--){
+        carry *= nums[i + 1];
+        result[i] *= carry;
+    }
 
     return result;
 }
