10640e6607bac8724afbafc62b1bcd453ab5d911
ailr16 <angel_ivan_5@outlook.com>
2025-07-21 00:40:39 -0600

    Refactoed atoi and meets all leetcode reqs

--- leetCode/src/p8.cpp
+++ leetCode/src/p8.cpp
@@ -1,27 +1,35 @@
 #include "p8.hpp"
 
 int p8::Solution::myAtoi(std::string s){
-    int result = 0;
-    bool signFlag = false;
+    long long result = 0;
+    int sign = 1;
     bool startFlag = false;
 
     for(char c : s){
-        if(c == ' ') continue;
-        if(c == '-') signFlag = true;
         if(!startFlag){
-            if(c > '0') startFlag = true;
+            if(c == ' ') continue;
+            if(c == '.') return 0;
+            else if(c == '+' || c == '-'){
+                startFlag = true;
+                if(c == '-') sign = -1;
+                continue;
+            }
+            else if(c >= '0') startFlag = true;
         }
+
         if(startFlag){
+            if(c == ' ') break;
             if(c >= '0' && c <= '9'){
                 result *= 10;
                 result += c - '0';
+
+                if(result * sign <= INT_MIN) return INT_MIN;
+                if(result * sign >= INT_MAX) return INT_MAX;
             }
-            else{
-                break;
-            }
+            else break;
         }
     }
 
-    if(signFlag) result *= -1;
+    result *= sign;
     return result;
 }
--- leetCode/test/TEST__p8.cpp
+++ leetCode/test/TEST__p8.cpp
@@ -14,5 +14,15 @@ TEST(Problem8Test, testSolution){
     EXPECT_EQ(test.myAtoi("00005420a"), 5420);
     EXPECT_EQ(test.myAtoi(""), 0);
     EXPECT_EQ(test.myAtoi("abc"), 0);
-
+    EXPECT_EQ(test.myAtoi("   1"), 1);
+    EXPECT_EQ(test.myAtoi("   -1"), -1);
+    EXPECT_EQ(test.myAtoi("   +1"), 1);
+    EXPECT_EQ(test.myAtoi("   001"), 1);
+    EXPECT_EQ(test.myAtoi("   00100x"), 100);
+    EXPECT_EQ(test.myAtoi("abc"), 0);
+    EXPECT_EQ(test.myAtoi("0-1"), 0);
+    EXPECT_EQ(test.myAtoi("-91283472332"), INT_MIN);
+    EXPECT_EQ(test.myAtoi(".1"), 0);
+    EXPECT_EQ(test.myAtoi("+-12"), 0);
+    EXPECT_EQ(test.myAtoi("   +0 123"), 0);
 }
